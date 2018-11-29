d5ce759731e62bfe6507de66fa33b1cb0d47f425
Alexandru NOVAC <anovac@bitdefender.com>
2018-11-29 17:07:38 +0200

    read currency info from the api and added axios functionality

--- public/main.js
+++ public/main.js
@@ -2,11 +2,20 @@ window.addEventListener( 'load' , function() {
   var app = new Vue( {
     el: '#app',
     data: {
-      col: [
-        { name: "Sandel" ,  prop1: "Violet" , prop2: 29 },
-        { name: "Georgel" , prop1: "Magenta" , prop2: 24 },
-        { name: "Ionel" ,   prop1: "Mov" , prop2: 25 }
+      currency: [
       ]
+    },
+    created: function() {
+      axios.get( '/api/currency' )
+        .then((_response) => {
+          this.currency = _response.data.data;
+        } );
+    },
+
+    filters: {
+      number: function( _in ) {
+        return _in.toFixed(2)
+      }
     }
   } );
 } );
