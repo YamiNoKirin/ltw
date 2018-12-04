78af5d60f0182d60dcd2e038ecf04ac6f23e7cbe
Alexandru NOVAC <anovac@bitdefender.com>
2018-12-04 17:16:49 +0200

    added server logs

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ app.use( bodyParser.json() );
 app.get( '/api/:type' , (_req , _res) => {
   const type = _req.params.type.toLowerCase();
   if ( type in database ) {
+    console.log( `[OK][GET] ${type} list` );
     _res.json( {
       ret: 'OK',
       data: database[type]
     } );
   } else {
+    console.log( `[ERROR][GET] list` );
     _res.json( {
       ret: 'ERROR',
       error: 'Entry not found'
@@ -29,11 +31,13 @@ app.get( '/api/:type/:id' , (_req , _res) => {
   const id = _req.params.id.toLowerCase();
   const type = _req.params.type.toLowerCase();
   if ( (type in database) && (id in database[type]) ) {
+    console.log( `[OK][GET] ${type}/${id}` );
     _res.json( {
       ret: 'OK',
       data: database[type][id]
     } );
   } else {
+    console.log( `[ERROR][GET] item` );
     _res.json( {
       ret: 'ERROR',
       error: 'Entry not found'
@@ -51,12 +55,14 @@ app.post( '/api/:type' , (_req , _res ) => {
     }
     const id = uuidv4();
     database[type][id] = _req.body;
+    console.log( `[OK][POST] ${type}` );
     _res.json( {
       ret: 'OK',
       id
     } );
     dbsync.save( database );
   } else {
+    console.log( `[ERROR][POST] item` );
     _res.json( {
       ret: 'ERROR',
       error: 'Invalid type format'
@@ -69,12 +75,14 @@ app.delete( '/api/:type/:id' , (_req , _res) => {
   const type = _req.params.type.toLowerCase();
   if ( (type in database) && (id in database[type]) ) {
     delete database[type][id];
+    console.log( `[OK][DELETE] ${type}/${id}` );
     _res.json( {
       ret: 'OK',
       data: `${type}/${id}`
     } );
     dbsync.save( database );
   } else {
+    console.log( `[ERROR][DELETE] item` );
     _res.json( {
       ret: 'ERROR',
       error: 'Entry not found'
@@ -87,12 +95,14 @@ app.put( '/api/:type/:id' , (_req , _res) => {
   const type = _req.params.type.toLowerCase();
   if ( (type in database) && (id in database[type]) ) {
     database[type][id] = _req.body;
+    console.log( `[OK][PUT] ${type}/${id}` );
     _res.json( {
       ret: 'OK',
       data: '${type}/${id}'
     } );
     dbsync.save( database );
   } else {
+    console.log( `[ERROR][PUT] item` );
     _res.json( {
       ret: 'ERROR',
       error: 'Entry not found'
